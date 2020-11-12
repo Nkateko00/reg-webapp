166aa5206e881a2a3b1daa7e22f674971d9cf35f
Nkateko00 <tekonku@icloud.com>
2020-11-12 15:03:46 +0200

    configuring functionality

--- routes.js
+++ routes.js
@@ -10,13 +10,13 @@ module.exports = function registrationRoutes(reg) {
     async function addReg(req, res) {
         var numb = req.body.regData
         var upperCase = numb.toUpperCase()
-        var allRegistrations = await reg.allRegistration()
+       
 
-     
 
         if (upperCase !== "") {
             
             if (/C[AYJ] \d{3,6}$/.test(upperCase) || /C[AYJ] \d{3}-\d{3}$/.test(upperCase)) {
+
                 if (await reg.regCheck(upperCase) === 0) {
                     await reg.addRegistration(upperCase)
                     req.flash('success', 'Succesful!')
@@ -29,15 +29,16 @@ module.exports = function registrationRoutes(reg) {
             else {
                 req.flash('error', 'Please enter a valid registration!')
             }
+            
         }
         else {
             req.flash('error', 'Please enter a registration!')
         }
-
+        var allRegistrations = await reg.allRegistration()
         res.render('index', {
             regNumb: allRegistrations
         });
-    }
+        }
     async function filterAll(req, res) {
         var filter = req.query.filter
         //from handlebars selection 
